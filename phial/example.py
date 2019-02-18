98f88f504bc4d0f48947135b6a80ceb0eb3d46ba
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-02-18 06:32:08 -0800

    Brought naming conventions back to standard.

--- phial/example.py
+++ phial/example.py
@@ -2,16 +2,16 @@
 """An example app using the Phial framework."""
 from phial import Router, Phial, Response
 
-ROUTER = Router()
+router = Router()
 
-@ROUTER.route(r'^/$')
+@router.route(r'^/$')
 async def say_hello(request):
     """Say hello to the world!!!"""
     return Response("Hello World", content_type="text/plain")
 
-@ROUTER.route(r'^/(?P<name>[\w]*)/?$')
+@router.route(r'^/(?P<name>[\w]*)/?$')
 async def hello_name(request, name: str):
     """Say hello based on names."""
     return Response(f"Hello {name}", content_type="text/plain")
 
-app = Phial(router=ROUTER)
+app = Phial(router=router)
